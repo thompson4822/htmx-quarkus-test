5582b6a65800e7d3289c0205220bbbb2f566649a
Steve Thompson <thompson4822@proton.me>
2025-09-15 20:22:59 -0600

    Created a basic page for Udemy content

---
+++ src/main/kotlin/com/sthompson/udemy/UdemyResource.kt
@@ -0,0 +1,32 @@
+package com.sthompson.udemy
+
+import io.quarkus.qute.Location
+
+import io.quarkus.qute.Template
+import io.quarkus.qute.TemplateInstance
+import jakarta.ws.rs.GET
+import jakarta.ws.rs.Path
+import jakarta.ws.rs.Produces
+import jakarta.ws.rs.core.MediaType
+
+@Path("/udemy")
+class UdemyResource(@Location("udemy/udemy.html") private val udemy: Template) {
+
+    @Location("udemy/_learn-more.qute.html")
+    lateinit var learnMoreTemplate: Template
+
+ 
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    fun get(): TemplateInstance {
+        return udemy.instance()
+    }
+    
+    @GET
+    @Path("/learn-more")
+    @Produces(MediaType.TEXT_HTML)
+    fun learnMore(): TemplateInstance {
+        return learnMoreTemplate.instance()
+    }
+}
